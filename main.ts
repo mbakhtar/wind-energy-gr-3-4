ce74a1362d1303f5200c47156fe75ea9c99f825a
bilal-inksmith <109742973+mbakhtar@users.noreply.github.com>
2023-10-27 14:44:20 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+fwdSensors.touch.fwdOnTouch(jacdac.ButtonEvent.Down, function () {
+    fwdMotors.servo1.fwdSetSpeed(0)
+})
+fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CCW, function (difference) {
+    fwdMotors.servo1.fwdSetSpeed(fwdSensors.dial1.fwdPosition())
+})
+fwdSensors.dial1.fwdOnDialTurned(fwdSensors.DialDirection.CW, function (difference) {
+    fwdMotors.servo1.fwdSetSpeed(fwdSensors.dial1.fwdPosition())
+})
